d60a3f990a052a7973f3a463133b9fe397633f57
ShadowGhost23714 <shadowghost23714@gmail.com>
2025-05-14 21:45:32 -0300

    Actuliz. ej7p2 y agregar ej8.p2

--- Practica02/Ejercicio07.go
+++ Practica02/Ejercicio07.go
@@ -26,45 +26,57 @@ func main() {
 	letras := 0
 	numeros := 0
 	especiales := 0
+	mayus := 0
+	minus := 0
+	m := make(map[string]int)
 	reader := bufio.NewReader(os.Stdin) // Crea un lector para leer texto desde la entrada estándar (teclado)
 	var ultimo rune
 	for !salir {
 		fmt.Print("Ingrese un caracter: ")
-		input, _ := reader.ReadString('\n') // Lee una línea completa hasta que se presiona ENTER
-		input = strings.TrimSpace(input)    // Elimina espacios y saltos de línea del inicio y fin
-		char := rune(input[0])              // Toma el primer carácter ingresado y lo convierte a rune (carácter Unicode)
+		letra, _ := reader.ReadString('\n')    // Lee una línea completa hasta que se presiona ENTER
+		letra = strings.TrimRight(letra, "\n") // Borra del string el salto de linea
+		char := rune(letra[0])                 // Toma el primer carácter ingresado y lo convierte a rune (carácter Unicode)
 		switch {
 		case unicode.IsLetter(char):
 			{
 				letras++
+				if unicode.IsUpper(char) {
+					mayus++
+				} else {
+					minus++
+				}
 				if ultimo == 'C' && char == 'R' {
 					salir = true
 				}
 			}
 		case unicode.IsDigit(char):
+			m[string(char)]++ // Puede ser también m[letra]++
 			numeros++
 		default:
 			especiales++
 		}
 		ultimo = char
 	}
-	fmt.Println(letras)
-	fmt.Println(numeros)
-	fmt.Println(especiales)
+	fmt.Println("Letras = ", letras)
+	fmt.Println("Números = ", numeros)
+	fmt.Println("Caracteres especiales = ", especiales)
+	fmt.Println("Mayúsculas = ", mayus)
+	fmt.Println("Minúsculas = ", minus)
+	fmt.Print(m)
 }
 
 /*
-func main() {
+func Antiguo() { // Versión vieja del jercicio viejo
 	pri := false
 	salir := false
 	letras := 0
 	numeros := 0
 	especiales := 0
-	reader := bufio.NewReader(os.Stdin)
+	reader := bufio.NewReader(os.Stdin) // Crea un lector para leer texto desde la entrada estándar (teclado)
 	for !salir {
-		fmt.Print("Ingrese un caracter: ")
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSpace(input)
+		fmt.Print("Ingrese un caracter: ")  // Lee una línea completa hasta que se presiona ENTER
+		input, _ := reader.ReadString('\n') // Elimina espacios y saltos de línea del inicio y fin
+		input = strings.TrimSpace(input)    // Toma el primer carácter ingresado y lo convierte a rune (carácter Unicode)
 		char := rune(input[0])
 		switch {
 		case unicode.IsLetter(char):
@@ -84,8 +96,8 @@ func main() {
 			especiales++
 		}
 	}
-	fmt.Println(letras)
-	fmt.Println(numeros)
-	fmt.Println(especiales)
+	fmt.Println("Letras = ", letras)
+	fmt.Println("Números = ", numeros)
+	fmt.Println("Caracteres especiales = ", especiales)
 }
 */
---
+++ Practica02/Ejercicio08.go
@@ -0,0 +1,93 @@
+/*
+8. Escriba un programa que implemente la función:
+
+func Convert ( v int , b int ) string
+
+a. La función debe convertir el entero v, en un string en su representación
+en base b. El string será el valor de retorno. Por ejemplo si se invoca:
+
+s = Convert ( 23 , 2 )
+
+En s se almacenaría el valor “10111”.
+
+Operación:
+
+25 / 8 = 3, residuo 1
+3 / 8 = 0, residuo 3
+El número en base 8 es 31.
+
+La base debe ser mayor que 1 y menor que 37 dado que irı́a de base-2
+hasta base-36, que usarı́a los dígitos:
+
+“0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ”
+
+b. Re-implemente la función considerando que v puede ser negativo, y se
+empleará el mismo símbolo (-) para su representación en base-b.
+*/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+const digitos = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+func Convert4(v int, b int) string { // antigüa solución
+	cociente := v / b
+	residuo := v % b
+	if cociente == 0 {
+		return strconv.Itoa(residuo)
+	}
+	return Convert3(cociente, b) + strconv.Itoa(residuo)
+}
+
+func Convert3(v int, b int) string { // antigüa solución
+	if b < 2 || b > 36 {
+		fmt.Println("Base no válida")
+	}
+	if v == 0 {
+		return ""
+	}
+	return Convert2(v/b, b) + string(digitos[v%b])
+}
+
+func Convert2(v int, b int) string { // Punto A
+	if b < 2 || b > 36 {
+		fmt.Println("Base no válida")
+	}
+	var resultado string
+	for v > 0 {
+		resultado = string(digitos[v%b]) + resultado
+		v = v / b
+	}
+	return resultado
+}
+
+func Convert(v int, b int) string { // Punto B
+	if b < 2 || b > 36 {
+		fmt.Println("Base no válida")
+	}
+	var resultado string
+	negativo := false
+	if v < 0 {
+		v = -v
+		negativo = true
+	}
+	for v > 0 {
+		resultado = string(digitos[v%b]) + resultado
+		v = v / b
+	}
+	if negativo {
+		resultado = "-" + resultado
+	}
+	return resultado
+}
+
+func main() {
+	fmt.Println("Número 25 y base 8 = ", Convert(25, 8))
+	fmt.Println("Número -25 y base 8 = ", Convert(-25, 8))
+	fmt.Println("Número 23 y base 2 = ", Convert(23, 2))
+	fmt.Println("Número -23 y base 2 = ", Convert(-23, 2))
+}
