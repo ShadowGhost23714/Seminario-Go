c1aaf881ac1b1bf8fcda73351e8e63de4f873c3c
ShadowGhost23714 <shadowghost23714@gmail.com>
2025-06-20 16:32:58 -0300

    Subiendo verFinal de eje3 p3

--- Practica03/Ejercicio03Obligatorio/VersionFinal/Ejercicio03Obligatorio.go
+++ Practica03/Ejercicio03Obligatorio/VersionFinal/Ejercicio03Obligatorio.go
@@ -36,106 +36,6 @@ var (
 	wg1 sync.WaitGroup
 )
 
-func worker(file0, file1 *os.File, wg *sync.WaitGroup) {
-	defer wg.Done()
-	ok1 := true
-	finalRound := false
-
-	for {
-
-		if !ok && !finalRound {
-			finalRound = true
-		} else if !ok && finalRound {
-			break
-		}
-
-		ok1 = true
-		for ok1 {
-			select {
-			case tarea := <-prio0Chan:
-				{
-					muTrabajos.Lock()
-					trabajosEnCero++
-					muTrabajos.Unlock()
-
-					res := sumarDigitos(tarea.num)
-					time.Sleep(5 * time.Second)
-					fmt.Println("prioridad 0, valor de la tarea, resultado: ", tarea.num, " ", res)
-					mut0.Lock()
-					file0.WriteString(fmt.Sprintf("(0, %d)\n", res))
-					mut0.Unlock()
-
-					muTrabajos.Lock()
-					trabajosEnCero--
-					muTrabajos.Unlock()
-				}
-			default:
-				for {
-					muTrabajos.Lock()
-					if trabajosEnCero == 0 {
-						muTrabajos.Unlock()
-						break
-					}
-					muTrabajos.Unlock()
-					time.Sleep(200 * time.Millisecond)
-				}
-
-				select {
-				case tarea := <-prio1Chan:
-					str := invertirDigitos(tarea.num)
-					fmt.Println("prioridad 1, valor de la tarea, resultado: ", tarea.num, " ", str, " ")
-					mut1.Lock()
-					file1.WriteString(fmt.Sprintf("(1, %s)\n", str))
-					mut1.Unlock()
-				default:
-					select {
-					case tarea := <-prio2Chan:
-						res := tarea.num * 10
-						fmt.Println("prioridad 2, valor de la tarea, resultado: ", tarea.num, " ", res)
-					default:
-						select {
-						case tarea := <-prio3Chan:
-							muAcum.Lock()
-							acumulado += tarea.num
-							fmt.Println("prioridad 3, valor de la tarea, resultado: ", tarea.num, " ", acumulado)
-							muAcum.Unlock()
-						default:
-							ok1 = false
-						}
-					}
-				}
-			}
-		}
-	}
-}
-
-func scheduler(done chan int) {
-	wg1.Add(1)
-	for {
-		select {
-		case <-done:
-			wg1.Done()
-			return
-		default:
-			num := rand.Intn(900) + 100
-			prio := rand.Intn(4)
-			t := Tarea{num: num, prioridad: prio}
-
-			switch prio {
-			case 0:
-				prio0Chan <- t
-			case 1:
-				prio1Chan <- t
-			case 2:
-				prio2Chan <- t
-			case 3:
-				prio3Chan <- t
-			}
-
-		}
-	}
-}
-
 func sumarDigitos(n int) int {
 	suma := 0
 	for n > 0 {
@@ -155,12 +55,121 @@ func invertirDigitos(numero int) string {
 	return resultado
 }
 
+func esperar() {
+	for {
+		muTrabajos.Lock()
+		if trabajosEnCero == 0 {
+			muTrabajos.Unlock()
+			break
+		}
+		muTrabajos.Unlock()
+		time.Sleep(200 * time.Millisecond)
+	}
+}
+
+func prioridad0(num int, file0 *os.File) {
+	muTrabajos.Lock()
+	trabajosEnCero++
+	muTrabajos.Unlock()
+
+	res := sumarDigitos(num)
+	time.Sleep(5 * time.Second)
+	fmt.Println("prioridad 0, valor de la tarea, resultado: ", num, " ", res)
+	mut0.Lock()
+	file0.WriteString(fmt.Sprintf("(0, %d)\n", res))
+	mut0.Unlock()
+
+	muTrabajos.Lock()
+	trabajosEnCero--
+	muTrabajos.Unlock()
+}
+
+func prioridad1(num int, file1 *os.File) {
+	str := invertirDigitos(num)
+	fmt.Println("prioridad 1, valor de la tarea, resultado: ", num, " ", str, " ")
+	mut1.Lock()
+	file1.WriteString(fmt.Sprintf("(1, %s)\n", str))
+	mut1.Unlock()
+}
+
+func prioridad2(num int) {
+	res := num * 10
+	fmt.Println("prioridad 2, valor de la tarea, resultado: ", num, " ", res)
+}
+
+func prioridad3(num int) {
+	muAcum.Lock()
+	acumulado += num
+	fmt.Println("prioridad 3, valor de la tarea, resultado: ", num, " ", acumulado)
+	muAcum.Unlock()
+}
+
+func worker(file0, file1 *os.File, wg *sync.WaitGroup) {
+	defer wg.Done()
+	ok1, finalRound := false, false
+	for {
+		if !ok && !finalRound {
+			finalRound = true
+		} else if !ok && finalRound {
+			break
+		}
+		ok1 = true
+		for ok1 {
+			select {
+			case tarea := <-prio0Chan:
+				prioridad0(tarea.num, file0)
+			default:
+				esperar()
+				select {
+				case tarea := <-prio1Chan:
+					prioridad1(tarea.num, file1)
+				default:
+					select {
+					case tarea := <-prio2Chan:
+						prioridad2(tarea.num)
+					default:
+						select {
+						case tarea := <-prio3Chan:
+							prioridad3(tarea.num)
+						default:
+							ok1 = false
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
+func scheduler(done chan int) {
+	wg1.Add(1)
+	for {
+		select {
+		case <-done:
+			wg1.Done()
+			return
+		default:
+			t := Tarea{num: rand.Intn(900) + 100, prioridad: rand.Intn(4)}
+			switch t.prioridad {
+			case 0:
+				prio0Chan <- t
+			case 1:
+				prio1Chan <- t
+			case 2:
+				prio2Chan <- t
+			case 3:
+				prio3Chan <- t
+			}
+
+		}
+	}
+}
+
 func main() {
 	ok = true
 
 	file0, _ := os.Create("prioridad0.txt")
 	defer file0.Close()
-
 	file1, _ := os.Create("prioridad1.txt")
 	defer file1.Close()
 
@@ -168,7 +177,7 @@ func main() {
 
 	var wg sync.WaitGroup
 
-	for i := 0; i < 4; i++ {
+	for range 4 {
 		wg.Add(1)
 		go worker(file0, file1, &wg)
 	}
@@ -184,8 +193,7 @@ func main() {
 	wg.Wait()
 
 	/*
-
-			La variable finalRound permite que los workers sigan procesando tareas después de que el scheduler se detiene (luego de 30 segundos).Para garantizar que todas las tareas generadas sean procesadas.
+		La variable finalRound permite que los workers sigan procesando tareas después de que el scheduler se detiene (luego de 30 segundos).Para garantizar que todas las tareas generadas sean procesadas.
 		Por qué no alcanza con ok1 = false?
 		Aunque los workers ponen ok1 = false cuando todas las colas están vacías, puede suceder que:
 
@@ -200,7 +208,5 @@ func main() {
 		Se puede evitar finalRound?
 		Solo si el objetivo es que tanto generación como procesamiento ocurran solo durante los 30 segundos. En ese caso, se debería forzar que todo termine al mismo tiempo,
 		incluso si quedan tareas sin procesar, se cierran los canales sin importar si aun tienen datos dentro.
-
 	*/
-
 }
